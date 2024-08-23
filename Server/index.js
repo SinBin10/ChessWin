a37cc83851d7f1387452fc4c9882c6055547ad84
SinBin10 <bincode10@gmail.com>
2024-08-23 19:18:39 +0530

    implementing socket.io

--- Server/index.js
+++ Server/index.js
@@ -2,12 +2,22 @@ const express = require("express");
 //explicitly creating the server so that we can have more control over it,
 //the functionality is the same as app.listen(creates a server as well)
 const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected...");
+  socket.on("disconnect", () => {
+    console.log("user disconncted...");
+  });
 });
 
 server.listen(3000, () => {
