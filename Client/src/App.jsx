a06379d2c0cddc5b6ac2a121f97707eea08f219d
SinBin10 <bincode10@gmail.com>
2024-08-25 03:08:16 +0530

    made the chessboard

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
+import { Chess } from "chess.js";
 
 const App = () => {
   useEffect(() => {
@@ -8,7 +9,32 @@ const App = () => {
       socket.disconnect();
     };
   }, []);
-  return <h1>Socket connected</h1>;
+  let draggedPiece = null;
+  let sourceSquare = null;
+  let playerRole = null;
+  const chess = new Chess();
+  return (
+    <>
+      <div className="w-full min-h-full flex items-center justify-center bg-slate-900">
+        <div className="w-[32rem] h-[32rem]">
+          {chess.board().map((row, rowIndex) => (
+            <div key={rowIndex} className="w-full flex">
+              {row.map((col, colIndex) => (
+                <div
+                  key={`${rowIndex}-${colIndex}`}
+                  className={`h-16 w-16 ${
+                    (rowIndex + colIndex) % 2 === 0
+                      ? "bg-[#fbf5de]"
+                      : "bg-[#f8e7bb]"
+                  }`}
+                ></div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
