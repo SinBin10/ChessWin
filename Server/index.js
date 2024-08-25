9f21a35982f71f5df537fb61832a97f70bbd5839
SinBin10 <bincode10@gmail.com>
2024-08-25 22:03:03 +0530

    building for deployment.

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ let players = {};
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // The Vite dev server address
+    origin: "https://chess-win.vercel.app/", // The Vite dev server address
     methods: ["GET", "POST"],
   },
 });
