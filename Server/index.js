a3227ec48aadfad4898e4aca1e1b0ad695336397
SinBin10 <bincode10@gmail.com>
2024-08-28 06:52:16 +0530

    handling backend changes for different rooms.

--- Server/index.js
+++ Server/index.js
@@ -8,13 +8,12 @@ const { v4: uuidv4 } = require("uuid");
 
 const path = require("path");
 let roomId = uuidv4();
+let tempRoomId = roomId;
 const app = express();
 const server = createServer(app);
-const chess = new Chess();
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
-let players = {};
 let games = {};
 
 const io = new Server(server, {
@@ -29,72 +28,86 @@ const io = new Server(server, {
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname1, "Client", "dist", "index.html"));
 // });
-
 io.on("connection", (socket) => {
   console.log("server connected...");
-  if (!games[roomId]?.white) {
-    games[roomId] = { white: socket.id };
-    socket.join(roomId);
-    console.log(`Player ${games[roomId].white} has joined the game.`);
+  console.log("temproomId", tempRoomId);
+  socket.data.roomId = tempRoomId;
+  if (!games[tempRoomId]?.white) {
+    socket.data.tempRoomId = tempRoomId;
+    games[tempRoomId] = { white: socket.id };
+    games[tempRoomId] = { ...games[tempRoomId], chess: new Chess() };
+    socket.join(tempRoomId);
+    console.log(`Player ${games[tempRoomId].white} has joined the game.`);
     // socket
-    //   .to(roomId)
+    //   .to(tempRoomId)
     //   .emit("message", `Player ${socket.id} has joined the game.`);
-    console.log(`white has joined the room ${roomId}`);
+    console.log(`white has joined the room ${tempRoomId}`);
     console.log(games);
     socket.emit("playerRole", "w");
-  } else if (!games[roomId]?.black) {
-    games[roomId] = { ...games[roomId], black: socket.id };
+  } else if (!games[tempRoomId]?.black) {
+    games[tempRoomId] = { ...games[tempRoomId], black: socket.id };
     socket.emit("playerRole", "b");
-    socket.join(roomId);
+    socket.join(tempRoomId);
     // socket
-    //   .to(roomId)
+    //   .to(tempRoomId)
     //   .emit("message", `Player ${socket.id} has joined the game.`);
-    console.log(`black has joined the room ${roomId}`);
+    console.log(`black has joined the room ${tempRoomId}`);
     console.log(games);
-    io.to(roomId).emit("bothPlayersConnected");
+    io.to(tempRoomId).emit("bothPlayersConnected");
     // io.emit("bothPlayersConnected");
     // players = {};
-    roomId = uuidv4();
+    tempRoomId = uuidv4();
+    console.log("after black joins room id", tempRoomId);
   }
-
-  socket.to(roomId).emit("boardState", chess.fen());
-  // socket.emit("boardState", chess.fen());
-
+  roomId = socket.data.roomId;
+  // console.log(games[roomId]);
+  socket.to(roomId).emit("boardState", games[roomId].chess.fen());
+  // socket.emit("boardState", games[roomId].chess.fen());
   // console.log(players);
   socket.on("disconnect", () => {
     console.log(roomId);
-    if (socket.id === games[roomId].white) {
+    if (socket.id === games[roomId]?.white) {
       delete games[roomId].white;
       console.log("player white left...");
-    } else if (socket.id === games[roomId].black) {
+    } else if (socket.id === games[roomId]?.black) {
       delete games[roomId].black;
       console.log("player black left...");
     }
+    console.log(games);
   });
   socket.on("move", (move) => {
+    console.log("this move is made for the game : ", roomId);
     try {
       // During white's turn black cannot move and vice versa
-      if (chess.turn() === "w" && socket.id !== players.white) return;
-      if (chess.turn() === "b" && socket.id !== players.black) return;
-      let result = chess.move(move);
+      if (
+        games[roomId].chess.turn() === "w" &&
+        games[roomId]?.id !== games[roomId].white
+      )
+        return;
+      if (
+        games[roomId].chess.turn() === "b" &&
+        games[roomId]?.id !== games[roomId].white
+      )
+        return;
+      let result = games[roomId].chess.move(move);
       if (result) {
-        io.emit("move", move);
-        if (chess.isGameOver()) {
-          io.emit("over", chess.turn());
-          delete players.white;
-          delete players.black;
-          io.emit("boardState", chess.fen());
-          chess.reset();
+        io.to(roomId).emit("move", move);
+        if (games[roomId].chess.isGameOver()) {
+          io.to(roomId).emit("over", games[roomId].chess.turn());
+          delete games[roomId]?.white;
+          delete games[roomId]?.black;
+          io.to(roomId).emit("boardState", games[roomId].chess.fen());
+          games[roomId].chess.reset();
         } else {
-          io.emit("boardState", chess.fen());
+          io.to(roomId).emit("boardState", games[roomId].chess.fen());
         }
       } else {
         console.log("invalid move...");
-        socket.emit("invalid move", move);
+        socket.to(roomId).emit("invalid move", move);
       }
     } catch (err) {
       console.log(err);
-      socket.emit("invalid move", move);
+      socket.to(roomId).emit("invalid move", move);
     }
   });
 });
