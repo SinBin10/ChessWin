d4b8490d8146550dfd8251b4f410a01dad9d269f
SinBin10 <bincode10@gmail.com>
2024-08-29 21:20:00 +0530

    made the home page.

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h1>This is the home page</h1>;
+  return (
+    <div className="h-full bg-slate-900 flex flex-col justify-center items-center">
+      <div>
+        <Link to="/path">
+          <button className="bg-blue-600 p-2 rounded-xl text-white mb-8">
+            Play with a friend
+          </button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/path">
+          <button className="bg-blue-600 p-2 rounded-xl text-white">
+            Play as Guest
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
