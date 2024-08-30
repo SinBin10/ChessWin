05d3dd746771a0ebcc5c0f1d54feab0503e27ff3
SinBin10 <bincode10@gmail.com>
2024-08-30 22:02:05 +0530

    changing paths.

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <div className="h-full bg-slate-900 flex flex-col justify-center items-center">
       <div>
-        <Link to="/path">
+        <Link to="/play">
           <button className="bg-blue-600 p-2 rounded-xl text-white mb-8">
             Play with a friend
           </button>
         </Link>
       </div>
       <div>
-        <Link to="/path">
+        <Link to="/play">
           <button className="bg-blue-600 p-2 rounded-xl text-white">
             Play as Guest
           </button>
